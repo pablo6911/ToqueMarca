6c7e5118d1fad539ba06e1819cd63add6efdbd2a
Fernandez <pablitopak@gmail.com>
2021-03-08 12:23:00 -0500

    Registro Marca

---
+++ bd/encriptarPassword.go
@@ -0,0 +1,13 @@
+package bd
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+//EncriptarPassword me permite encryptar--------
+func EncriptarPassword(pass string) (string, error) {
+	costo := 8
+	bytes, err := bcrypt.GenerateFromPassword([]byte(pass), costo)
+	return string(bytes), err
+
+}
---
+++ models/usuario.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//Usuario es el modelo de usuario de la base de MongoDB--------
+type Usuario struct {
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Nombre          string             `bson:"nombre" json:"nombre,omitempty"`
+	FechaNacimiento time.Time          `bson:"fechaNacimiento" json:"fechaNacimiento,omitempty"`
+	Email           string             `bson:"email" json:"email"`
+	Password        string             `bson:"password" json:"password,omitempty"`
+	Avatar          string             `bson:"avatar" json:"avatar,omitempty"`
+	Ubicacion       string             `bson:"ubicacion" json:"ubicacion,omitempty"`
+}
