f5228c28d6cc8ce14fafd48cd613b6f6a3dbb502
Fernandez <pablitopak@gmail.com>
2021-03-11 17:18:55 -0500

    JWT

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -27,3 +27,26 @@ func GeneroJWT(t models.Usuario) (string, error) {
 	}
 	return tokenstr, nil
 }
+
+//GeneroJWTVideo genera el encriptado con JWT-----------
+func GeneroJWTVideo(t models.Video) (string, error) {
+
+	miClave := []byte("Toque")
+
+	payload := jwt.MapClaims{
+		"nombre":      t.Nombre,
+		"fechaSubida": t.FechaSubida,
+		"email":       t.Email,
+		"codigo":      t.Codigo,
+		"link":        t.Link,
+		"empresa":     t.Empresa,
+		"_id":         t.ID.Hex(),
+		"exp":         time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	tokenstr, err := token.SignedString(miClave)
+	if err != nil {
+		return tokenstr, err
+	}
+	return tokenstr, nil
+}
