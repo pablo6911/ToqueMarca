66e3a70d4f27375ffeebba33ef88a27472f3df4e
Fernandez <pablitopak@gmail.com>
2021-03-11 17:26:04 -0500

    HANDLERS

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -14,14 +14,25 @@ import (
 //Manejadores seteo mi puerto, el Handler y pongo a escuchar al servidor---------
 func Manejadores() {
 	router := mux.NewRouter()
-
+	//Registro video y marca
 	router.HandleFunc("/registro", middlew.ChequeoBD(routers.Registro)).Methods("POST")
 	router.HandleFunc("/registroVideo", middlew.ChequeoBD(routers.Video)).Methods("POST")
+
+	//login marca
 	router.HandleFunc("/login", middlew.ChequeoBD(routers.Login)).Methods("POST")
+
+	//ver perfil video y marac
 	router.HandleFunc("/verperfil", middlew.ChequeoBD(middlew.ValidoJWT(routers.Verperfil))).Methods("GET")
+
+	//modificar perfil video y maraca
 	router.HandleFunc("/modificarPerfil", middlew.ChequeoBD(middlew.ValidoJWT(routers.ModificoPerfil))).Methods("Put")
 
+	//Subir foto marca
 	router.HandleFunc("/subirAvatar", middlew.ChequeoBD(routers.SubirAvatar)).Methods("POST")
+
+	//subir video -->vieo
+
+	//obtener avatar marca
 	router.HandleFunc("/obtenerAvatar", middlew.ChequeoBD(routers.ObtenerAvatar)).Methods("GET")
 
 	PORT := os.Getenv("PORT")
